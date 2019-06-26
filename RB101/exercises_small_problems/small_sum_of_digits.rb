b6b679a08e8bd1858d7dc218ef624f6278f1247c
Lino Jimenez <linojr+linocodes@gmail.com>
2019-06-25 22:24:03 -0500

    Added solution to sum of digits. Added rps rubocop exercise.

--- RB101/exercises_small_problems/small_sum_of_digits.rb
+++ RB101/exercises_small_problems/small_sum_of_digits.rb
@@ -1,2 +1,7 @@
 def sum(number)
+  total = 0
+  number.to_s.split('').each { |el| total += el.to_i }
+  total
 end
+
+p sum(123_123)
---
+++ RB101/l21_rubocop_rps.rb
@@ -0,0 +1,49 @@
+VALID_CHOICES = %w(rock paper scissors)
+
+p VALID_CHOICES
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'paper' && second == 'rock') ||
+  (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  display_results(choice, computer_choice)
+
+  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thanks and goodbye.")
