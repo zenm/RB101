1566141c33ce373e66f6dd0b5a87edc56d6b878e
Lino Jimenez <linojr+linocodes@gmail.com>
2019-06-26 08:04:52 -0500

    Added bonus problem.

---
+++ RB101/exercises_small_problems/small_my_bonus.rb
@@ -0,0 +1,7 @@
+def calculate_bonus(amount, pays_out) 
+  bonus = pays_out ? amount / 2 : 0
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
