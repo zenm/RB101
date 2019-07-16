c07bd75d497923cddbb3ea5fd68cae5a5a536fb0
Lino Jimenez <ljimenez@bulls.com>
2019-07-16 12:49:50 -0500

    Attempted solution for str to signed number

--- RB101/exercises_small_problems/easy04/e04_08_str_signed_num.rb
+++ RB101/exercises_small_problems/easy04/e04_08_str_signed_num.rb
@@ -1,12 +1,29 @@
-DIGITS = {
-  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
-  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+HASH_NUMBERS = {
+  "0" => 0,
+  "1" => 1,
+  "2" => 2,
+  "3" => 3,
+  "4" => 4,
+  "5" => 5,
+  "6" => 6,
+  "7" => 7,
+  "8" => 8,
+  "9" => 9
 }
 
+def string_to_signed_integer(string_number)
+  array_str_numbers = string_number.split('')
+  if string_number.start_with?('-') || string_number.start_with?('+')
+    number_sign = array_str_numbers.shift
+  end
 
+  array_numbers = array_str_numbers.map { |n| HASH_NUMBERS[n] }
+  int = array_numbers.reduce { |total, num| total * 10 + num }
+  return int * -1 if number_sign == '-'
+  return int
+end
 
-# p string_to_integer(123)
-p string_to_integer('4321')
-p string_to_integer('570')
+p string_to_signed_integer('4321') == 4321
+p string_to_signed_integer('-570') == -570
+p string_to_signed_integer('+100') == 100
 
-p string_to_integer('-570')
