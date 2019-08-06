97037068c53556fd1f1b41ffd6708317b8ee8d31
Lino <linojr@gmail.com>
2019-08-06 07:46:04 -0500

    Added attempts at practice problems hard 01

---
+++ RB101/lesson_03/practice_problems_hard01_q01.rb
@@ -0,0 +1,10 @@
+
+# What do you expect to happen when the greeting variable is referenced in the last line of the code below?
+
+if false
+  greeting = “hello world”
+end
+
+greeting
+
+# My guess: name error
