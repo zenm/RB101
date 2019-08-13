754c7c15302d677213792955d7ccb13810be6190
Lino <linojr@gmail.com>
2019-08-13 08:04:58 -0500

    Added attempt to m01_02

--- RB101/exercises_small_problems/medium01/m01_02_rotation_part_2.rb
+++ RB101/exercises_small_problems/medium01/m01_02_rotation_part_2.rb
@@ -7,10 +7,24 @@
 
 # You may assume that n is always a positive integer.
 
-rotate_rightmost_digits(735291, 1) == 735291
-rotate_rightmost_digits(735291, 2) == 735219
-rotate_rightmost_digits(735291, 3) == 735912
-rotate_rightmost_digits(735291, 4) == 732915
-rotate_rightmost_digits(735291, 5) == 752913
-rotate_rightmost_digits(735291, 6) == 352917
+
+def rotate_array(array)
+  array[1..-1] + [array[0]]
+end
+
+def rotate_rightmost_digits(target_num, rotate_num)
+  negative_index = rotate_num * -1
+  array_to_rotate = target_num.to_s.split('')[negative_index..-1]
+  first_str = target_num.to_s.split('')[0..negative_index].join('')
+  rotate_str = rotate_array(array_to_rotate).join('')
+  first_str.concat(rotate_str).to_i
+
+end
+
+p rotate_rightmost_digits(735291, 1) #== 735291
+p rotate_rightmost_digits(735291, 2) #== 735219
+p rotate_rightmost_digits(735291, 3) #== 735912
+p rotate_rightmost_digits(735291, 4) #== 732915
+p rotate_rightmost_digits(735291, 5) #== 752913
+p rotate_rightmost_digits(735291, 6) #== 352917
 
