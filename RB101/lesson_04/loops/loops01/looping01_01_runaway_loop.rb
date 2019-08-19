c99ff84b39f3f04d8703d56ec28fffd2140c601a
Lino Jimenez <linojr+linocodes@gmail.com>
2019-08-18 21:12:27 -0500

    Added looping exercises, revisited.

---
+++ RB101/lesson_04/loops/loops01/looping01_01_runaway_loop.rb
@@ -0,0 +1,20 @@
+# The code below is an example of an infinite loop.
+# The name describes exactly what it does: loop infinitely.
+# This loop isn't useful in a real program, though.
+# Modify the code so the loop stops after the first iteration.
+
+# loop do
+#   puts 'Just keep printing...'
+# end
+
+# input
+# output:
+# nil
+# outputs 'Just keep printing...'
+# requirements:
+# break the loop after one iteration
+
+loop do
+  puts 'Just keep printing...'
+  break
+end
---
+++ RB101/lesson_04/loops/loops01/looping01_03_control_the_loop.rb
@@ -0,0 +1,9 @@
+# Modify the following loop so it iterates 5 times instead of just once.
+
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+  break if iterations > 5
+end
